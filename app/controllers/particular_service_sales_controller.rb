bdf10bbff16aa9fa733663d6345d520e1c10a69c
jeison castro <jeisoncastro2@gmail.com>
2014-12-09 17:17:42 -0500

    rutas restantes por corregir

--- app/controllers/particular_service_sales_controller.rb
+++ app/controllers/particular_service_sales_controller.rb
@@ -20,7 +20,7 @@ class ParticularServiceSalesController < ApplicationController
   end
     
   def ready_sales
-     @service = 0
+       @service = 0
     @i_service = InsuredService.find(params[:id_sale])
     @codes = to_hash_code(Service.where(clinic_area_id: @i_service.clinic_area_id).order(:id))
     if @i_service.clinic_area_id == 6
@@ -29,11 +29,27 @@ class ParticularServiceSalesController < ApplicationController
       @services = Service.where(clinic_area_id: @i_service.clinic_area_id).order(:name)
     end
     
-    @clinic_area = ClinicArea.find(@i_service.clinic_area_id)
+    @clinic_area = Cli nicArea.find(@i_service.clinic_area_id)
     @authorization = Authorization.find(@i_service.authorization_id)
     @service_exenteds = to_hash(ServiceExented.all)
   end
 
   def show
+  end
+  def confirm_sale
+    if current_employee.area_id == 6
+      i = InsuredService.create(authorization_id: params[:id_authorization], clinic_area_id: ClinicArea.find_by_name(params[:name]).id, employee: current_employee, doctor_id: params[:doctor_id], has_ticket: false)
+    else
+      i = InsuredService.create(authorization_id: params[:id_authorization], clinic_area_id: ClinicArea.find_by_name(params[:name]).id, employee: current_employee, doctor_id: params[:doctor_id], has_ticket: true)
+    end
+    
+    redirect_to new_sales_ready_particular_path(id_sale: i.id)
+  end
+  def to_hash_code(query)
+    hash = Hash.new
+    query.each do |q|
+      hash[q.code] = q.id
+    end
+    hash
   end
 end
