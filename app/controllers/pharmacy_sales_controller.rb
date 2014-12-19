7ce84a1ba00697f8dded9f7b01e10d6da6ce40be
Fabian <f.pena.jacobo@gmail.com>
2014-12-19 14:55:51 -0500

    Corregida sentencia en porcentaje

--- app/controllers/pharmacy_sales_controller.rb
+++ app/controllers/pharmacy_sales_controller.rb
@@ -24,7 +24,7 @@ class PharmacySalesController < ApplicationController
   	@product_pharm_types = ProductPharmType.all
   	@digemid_products = get_digemid_hash(DigemidProduct.all.order(:name))
     @product_pharm_exenteds = to_hash(ProductPharmExented.all)
-    if @i_pharmacy.insurance.id = 2
+    if @pharm.authorization.patient.insured.insurance_id == 3
       @porc = "20%"
     else
       @porc = "10%"
