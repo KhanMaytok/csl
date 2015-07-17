2e482a495f6ae2fc471b6a84d6fa7e87d2f668a2
Fabian <f.pena.jacobo@gmail.com>
2015-07-17 14:24:46 -0500

    Retornada vista de empleadas a general

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,5 +1,5 @@
 class EmployeesController < ApplicationController
   def index
-  	@employees = Employee.where(area_id: 1).joins(:area).order("areas.id desc")
+  	@employees = Employee.all.joins(:area).order("areas.id desc")
   end
 end
