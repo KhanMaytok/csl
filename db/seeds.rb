f21b17f5b6ec45168346673bd441eb4d6f9c85f0
Fabian <f.pena.jacobo@gmail.com>
2015-09-29 18:22:16 -0500

    Agregado factor a positiva seguros

--- db/seeds.rb
+++ db/seeds.rb
@@ -780,6 +780,13 @@ i = Insurance.find(22)
 8.times do |t|
 	i.factors.create(clinic_area_id: t + 1, factor: 4.7)
 end
-=end
 
 PayDocument.all.update_all(not_export: false)
+
+=end
+
+
+i = Insurance.find(21)
+8.times do |t|
+	i.factors.create(clinic_area_id: t + 1, factor: 4.1)
+end
