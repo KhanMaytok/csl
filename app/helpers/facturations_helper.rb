47793463b2c75d62d526693022be068adb491636
Fabian <f.pena.jacobo@gmail.com>
2015-11-13 09:33:01 -0500

    Agregada visualización de estado en lista de facturas

--- app/helpers/facturations_helper.rb
+++ app/helpers/facturations_helper.rb
@@ -1,2 +1,14 @@
 module FacturationsHelper
+	def html_status(status)
+		case status
+		when 'R'
+			'Refacturada'
+		when 'N'
+			'Correcta'
+		when 'D'
+			'Anulada'
+		else
+			'Error'
+		end
+	end
 end
